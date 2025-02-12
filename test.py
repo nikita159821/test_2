6b69773f112d329459e79570c21b06c7958f096e
nikita159821 <132481753+nikita159821@users.noreply.github.com>
2025-02-12 17:56:18 +0500

    афафыаdsad

--- test.py
+++ test.py
@@ -1,46 +1,25 @@
-# import all required frameworks
-import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By  # Импортируем By
-from selenium.webdriver.firefox.options import Options  # Импортируем Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pytest
 
 
-# inherit TestCase Class and create a new test class
-class PythonOrgSearch(unittest.TestCase):
-
-    # initialization of webdriver
-    def setUp(self):
-        options = Options()
-        options.add_argument("--headless")  # Включаем headless режим
-        options.add_argument("--no-sandbox")
-        self.driver = webdriver.Chrome(options=options)  # Передаем options драйверу
-
-    # Test case method. It should always start with test_
-    def test_search_in_python_org(self):
-        # get driver
-        driver = self.driver
-        # get python.org using selenium
-        driver.get("http://www.python.org")
-
-        # assertion to confirm if title has python keyword in it
-        self.assertIn("Python", driver.title)
-
-        # locate element using name
-        elem = driver.find_element(By.NAME, "q")  # Используем By.NAME
-
-        # send data
-        elem.send_keys("pycon")
-
-        # receive data
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
-
-    # cleanup method called after every test performed
-    def tearDown(self):
-        self.driver.quit()  # Используем quit() вместо close()
+@pytest.fixture()
+def driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    browser = webdriver.Chrome(options=options)
+    return browser
 
 
-# execute the script
-if __name__ == "__main__":
-    unittest.main()
+def test_sale(driver):
+    driver.get('https://magento.softwaretestingboard.com/sale.html')
+    title = driver.find_element(By.TAG_NAME, 'h1')
+    assert title.text == 'Sale'
+
+
+def test_whats_new(driver):
+    driver.get('https://magento.softwaretestingboard.com/what-is-new.html')
+    title = driver.find_element(By.TAG_NAME, 'h1')
+    assert title.text == "What's New"
